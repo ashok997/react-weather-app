1c25a3ba21ca7eec989052c361bc515ea300c057
ashok997 <ashok997@gmail.com>
2020-10-09 14:09:04 -0500

    added search box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search...." />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
