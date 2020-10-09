936bafffefbf0d98c245334cb810dcc07c8a7af3
ashok997 <ashok997@gmail.com>
2020-10-09 14:21:47 -0500

    added a date parser to display current date

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 function App() {
+  const dateParser = (d) => {
+    let day = d.getDay();
+    let date = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+
+    return `${day}${date}${month}${year}`;
+  };
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search...." />
+        </div>
+        <div className="location-box">
+          <div className="location">San Antonio, US</div>
+          <div className="date">{dateParser(new Date())}</div>
         </div>
       </main>
     </div>
