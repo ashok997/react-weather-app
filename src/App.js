f7c178aa0b2210a0fa70398c320bbaf5fd1dfb57
ashok997 <ashok997@gmail.com>
2020-10-12 21:05:50 -0500

    create .env file to store api keys

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+const API_KEY = `${process.env.REACT_APP_WEATHER_API_KEY}`;
+const API_URL = "https://api/openweahermap.org/data/2.5";
+
 function App() {
   const dateParser = (d) => {
     const months = [
