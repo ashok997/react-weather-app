940f5a00dd2a56822dc289c3770c54bb238a47cc
ashok997 <ashok997@gmail.com>
2020-10-22 15:29:06 -0500

    updated to display location, temp. and weather from returned data object

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         .then((result) => {
           setWeather(result);
           setQuery("");
-          console.log(result);
         });
     }
   };
@@ -64,14 +63,22 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">San Antonio, US</div>
-          <div className="date">{dateParser(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15°</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateParser(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°F</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
